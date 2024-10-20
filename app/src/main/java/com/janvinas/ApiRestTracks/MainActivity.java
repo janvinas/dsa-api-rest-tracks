39332437278525be85996ded0875161c30bb46b9
Jan Viñas Corella <mirabetjan@gmail.com>
2024-10-20 12:05:06 +0200

    fix song create

--- app/src/main/java/com/janvinas/ApiRestTracks/MainActivity.java
+++ app/src/main/java/com/janvinas/ApiRestTracks/MainActivity.java
@@ -2,6 +2,9 @@ package com.janvinas.ApiRestTracks;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,28 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private void getSongs(){
+        RecyclerView recyclerView = findViewById(R.id.track_list);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        TracksService tracksService = NetworkClient.getInstance().tracksService;
+        tracksService.getTracks().enqueue(new Callback<List<Track>>() {
+            @Override
+            public void onResponse(Call<List<Track>> call, Response<List<Track>> response) {
+                RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(response.body());
+                myAdapter.setActivityInstance(MainActivity.this);
+                recyclerView.setAdapter(myAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Track>> call, Throwable t) {
+                Log.d("retrofit", "Failed to perform HTTP Request " + call.request().url());
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +56,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        RecyclerView recyclerView = findViewById(R.id.track_list);
+        getSongs();
+    }
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+    public void onCreateClick(View v){
+        Track track = new Track();
+        track.singer = ((TextView) findViewById(R.id.newsong_singer)).getText().toString();
+        track.title = ((TextView) findViewById(R.id.newsong_title)).getText().toString();
 
-            TracksService tracksService = NetworkClient.getInstance().tracksService;
-            tracksService.getTracks().enqueue(new Callback<List<Track>>() {
+        TracksService tracksService = NetworkClient.getInstance().tracksService;
+        tracksService.createTrack(track).enqueue(new Callback<Track>() {
             @Override
-            public void onResponse(Call<List<Track>> call, Response<List<Track>> response) {
-                RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(response.body());
-                myAdapter.setActivityInstance(MainActivity.this);
-                recyclerView.setAdapter(myAdapter);
+            public void onResponse(Call<Track> call, Response<Track> response) {
+                getSongs();
             }
 
             @Override
-            public void onFailure(Call<List<Track>> call, Throwable t) {
-                Log.d("retrofit", "Failed to perform HTTP Request " + call.request().url());
+            public void onFailure(Call<Track> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error performing request!", Toast.LENGTH_LONG).show();
             }
         });
     }
